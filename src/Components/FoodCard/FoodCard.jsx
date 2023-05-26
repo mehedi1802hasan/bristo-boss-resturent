1bb3d50aa9b7839c92fa5bd8c549b8482e14ca48
mehedi1802hasan <mehedi1802hasan@gmail.com>
2023-05-26 21:51:21 +0600

    menu data load from backend

--- src/Components/FoodCard/FoodCard.jsx
+++ src/Components/FoodCard/FoodCard.jsx
@@ -4,13 +4,13 @@ const FoodCard = ({item}) => {
     const {image,name,recipe,price}=item;
     return (
         <div className="card w-96 glass">
-        <figure><img class="w-72 " src={image} /></figure>
-        <div className="card-body">
+        <figure><img className='h-60' src={image} /></figure>
+        <div className=" card-body">
           <h2 className="text-3xl font-bold card-title">{name}</h2>
           <p>{recipe}</p>
           <i className='font-bold'>$ {price}</i>
           <div className=" card-actions">
-            <button className="btn btn-primary">Order</button>
+            <button className="border-0 border-b-4 border-red-600 btn btn-outline bg-slate-100">Order</button>
           </div>
         </div>
       </div>

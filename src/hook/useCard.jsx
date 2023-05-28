bb907c849bf4b9e1a2086058ab6ff43d20fde3cd
mehedi1802hasan <mehedi1802hasan@gmail.com>
2023-05-29 02:22:23 +0600

    setup tanstack query

---
+++ src/hook/useCard.jsx
@@ -0,0 +1,17 @@
+import { useQuery } from '@tanstack/react-query'
+import { useContext } from 'react';
+import { AuthContext } from '../provider/AuthProvider';
+const useCard=()=>{
+const {user}=useContext(AuthContext)
+const { refetch,  data:card=[] } = useQuery({
+queryKey: ['cards', user?.email],
+queryFn: async () => {
+  const res = await fetch(`http://localhost:3000/cards/${user?.email}`)
+ 
+  return res.json()
+
+}
+})
+return [card,refetch]
+}
+export default useCard;
